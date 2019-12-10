ebb59705bb466fd313e2da29b868431a1d15bec7
tgeery <tpgeery@socal.rr.com>
2019-12-09 23:28:20 -0800

    Update templates

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('daily/', include('daily.urls')),
     path('admin/', admin.site.urls),
 ]
--- daily/views.py
+++ daily/views.py
@@ -8,22 +8,29 @@ def index(request):
     return render(request, 'daily/index.html', {'entries':entries})
 
 def entry_1(request):
-    return render(request, 'daily/entry_1.html', {})
+    entry = Journal.objects.get(pk=1)
+    return render(request, 'daily/entry_1.html', {'entry':entry})
 
 def entry_2(request):
-    return render(request, 'daily/entry_2.html', {})
+    entry = Journal.objects.get(pk=2)
+    return render(request, 'daily/entry_2.html', {'entry':entry})
 
 def entry_3(request):
-    return render(request, 'daily/entry_3.html', {})
+    entry = Journal.objects.get(pk=3)
+    return render(request, 'daily/entry_3.html', {'entry':entry})
 
 def entry_4(request):
-    return render(request, 'daily/entry_4.html', {})
+    entry = Journal.objects.get(pk=4)
+    return render(request, 'daily/entry_4.html', {'entry':entry})
 
 def entry_5(request):
-    return render(request, 'daily/entry_5.html', {})
+    entry = Journal.objects.get(pk=5)
+    return render(request, 'daily/entry_5.html', {'entry':entry})
 
 def entry_6(request):
-    return render(request, 'daily/entry_6.html', {})
+    entry = Journal.objects.get(pk=6)
+    return render(request, 'daily/entry_6.html', {'entry':entry})
 
 def entry_7(request):
-    return render(request, 'daily/entry_7.html', {})
+    entry = Journal.objects.get(pk=7)
+    return render(request, 'daily/entry_7.html', {'entry':entry})
