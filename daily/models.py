0332d033a25b3b336bb8647472c0a5a49eea07a8
tgeery <tpgeery@socal.rr.com>
2020-01-04 15:25:09 -0800

    add highlight js format of code and terminal

--- daily/models.py
+++ daily/models.py
@@ -6,9 +6,3 @@ from django.db import models
 class Journal(models.Model):
     title = models.CharField(max_length=200, default='title')
     date = models.DateTimeField('date published', default=0)
-    header1 = models.CharField(max_length=200, default='')
-    context1 = models.CharField(max_length=200, default='', null=True, blank=True)
-    header2 = models.CharField(max_length=200, default='', null=True, blank=True)
-    context2 = models.CharField(max_length=200, default='', null=True, blank=True)
-    header3 = models.CharField(max_length=200, default='', null=True, blank=True)
-    context3 = models.CharField(max_length=200, default='', null=True, blank=True)
